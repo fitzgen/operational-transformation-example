0f2c4bc928b7480444693a1e5480d9121e9aedcb
Nick Fitzgerald <fitzgen@gmail.com>
2011-03-24 09:54:38 -0700

    Updating the clean.sh script

--- util/clean.sh
+++ util/clean.sh
@@ -5,14 +5,16 @@ set -e
 PWD=$(pwd)
 UTILDIR="$PWD/$(dirname $0)"
 ROOT=${UTILDIR/\/util/}
-
 OUTDIR="$ROOT/src/www/js"
+VENDORDIR="$ROOT/src/vendor"
+
 VERSION="1.6.0"
 RJSVERSION="0.24.0"
 
 DOJODIR="dojo-release-${VERSION}-src"
 REQUIREJSDIR="requirejs-${RJSVERSION}"
 
-rm -rf "$OUTDIR/operational-transformation"
 rm -rf "$OUTDIR/$DOJODIR"
-rm -rf "$OUTDIR/$REQUIREJSDIR"
+rm -rf "$OUTDIR/require"
+rm -rf "$VENDORDIR/requirejs-build"
+rm "$ROOT/src/r.js"
