6c7ee2c17d74bbe86803d6e842086cb0f145ea37
Nick Fitzgerald <fitzgen@gmail.com>
2011-03-28 00:01:36 -0700

    Exposing more functionality for the socket

--- src/www/js/app/socket.js
+++ src/www/js/app/socket.js
@@ -1,21 +1,60 @@
+/*jslint onevar: true, undef: true, eqeqeq: true, bitwise: true,
+  newcap: true, immed: true, nomen: false, white: false, plusplus: false,
+  laxbreak: true */
+
+/*global define, io */
+
 define([
     'dojo',
     '../socket.io.js'
 ], function (dojo) {
-    return function (callback) {
+    return function (callback, errback) {
+        console.log("Creating a new socket");
         var socket = new io.Socket();
-        socket.on("connect", function () {
-            callback(window.socket = {
+
+        socket.on("connect", function connector () {
+            console.log("Socket connected");
+            callback({
                 send: function (data) {
+                    console.log("Sending " + dojo.toJson(data));
                     socket.send(dojo.toJson(data));
                 },
-                receive: function (fn) {
-                    socket.on("message", function (event) {
-                        fn(dojo.fromJson(event.data));
+                onMessage: function (fn) {
+                    socket.on("message", function (data) {
+                        if ( typeof data === "string" ) {
+                            data = dojo.fromJson(data);
+                        }
+                        console.log("Received " + dojo.toJson(data));
+                        fn(data);
+                    });
+                },
+                onReconnect: function (fn) {
+                    socket.on("reconnect", function () {
+                        console.log("Reconnected");
+                        fn();
+                    });
+                },
+                onFailedReconnect: function (fn) {
+                    socket.on("reconnect_failed", function () {
+                        console.log("Failed to reconnect");
+                        fn();
+                    });
+                },
+                onDisconnect: function (fn) {
+                    socket.on("disconnect", function () {
+                        console.log("disconnected");
+                        fn();
                     });
                 }
             });
         });
+
+        socket.on("connect_failed", function () {
+            if ( errback ) {
+                errback();
+            }
+        });
+
         socket.connect();
     };
 });
